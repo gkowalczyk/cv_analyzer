e1a41be399c631f00b27646a95e4546eac4a7c21
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2024-11-01 14:55:44 +0100

    Initial commit of Spring Boot project structure

---
+++ src/test/java/com/example/cv_analyzer/CvAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cv_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
