522637ecd7003c5031fdb467ad8d1265604f1935
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-07-24 06:29:46 +0200

    fix(chat): add exception handling

--- src/main/java/com/example/cv_analyzer/controller/ChatController.java
+++ src/main/java/com/example/cv_analyzer/controller/ChatController.java
@@ -6,11 +6,14 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 
@@ -19,17 +22,23 @@ import java.util.List;
 public class ChatController {
 
     private final ChatModel chatModel;
+
     @CrossOrigin(origins = "*")
     @GetMapping("/chat")
     public ResponseEntity<String> chat(@RequestParam(value = "message") String message) {
-        UserMessage userMessage = new UserMessage(message);
+        try {
+            UserMessage userMessage = new UserMessage(message);
 
-        ChatResponse response = chatModel.call(new Prompt(List.of(userMessage),
-                OpenAiChatOptions
-                        .builder()
-                        .withFunction("getMyCv")
-                        .withFunction("getOffersAdapter")
-                        .build()));
-        return ResponseEntity.ok(response.getResult().getOutput().getContent());
+            ChatResponse response = chatModel.call(new Prompt(List.of(userMessage),
+                    OpenAiChatOptions
+                            .builder()
+                            .withFunction("getMyCv")
+                            .withFunction("getOffersAdapter")
+                            .build()));
+            return ResponseEntity.ok(response.getResult().getOutput().getContent());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error" + e.getMessage());
+        }
     }
 }
