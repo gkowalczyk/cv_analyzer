90edc47580d82a066f6a890fcc301108d615ac67
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-08-21 06:55:42 +0200

    editing ChromeHeadlessWebDriverProvider.java driver

--- src/main/java/com/example/cv_analyzer/infrastructure/scraper/justjoinit/ChromeHeadlessWebDriverProvider.java
+++ src/main/java/com/example/cv_analyzer/infrastructure/scraper/justjoinit/ChromeHeadlessWebDriverProvider.java
@@ -6,17 +6,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Component;
 import java.util.UUID;
 
+
 @Component
 public class ChromeHeadlessWebDriverProvider implements WebDriverProvider {
     @Override
     public WebDriver createWebDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
-     //   options.addArguments("--no-sandbox");
-      //  options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
-      //  options.addArguments("--user-data-dir=/tmp/chrome-profile-" + UUID.randomUUID());
+        options.addArguments("--user-data-dir=/tmp/chrome-profile-" + UUID.randomUUID());
         return new ChromeDriver(options);
     }
 }
